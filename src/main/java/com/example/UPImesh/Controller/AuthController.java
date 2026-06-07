5f5bc9c81e504c2b03c4adad518412c5b9acfbbc
AnuragjhA18-2005 <anuragjha0200@gmail.com>
2026-06-07 22:42:59 +0530

    feat: add AuthController for user authentication endpoints

---
+++ src/main/java/com/example/UPImesh/Controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.example.UPImesh.Controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.UPImesh.Security.JWTutil;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired private JWTutil jwTutil;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String,String> credentials){
+        String username = credentials.get("username");
+        String password = credentials.get("password");
+
+        if ("bridge-node-1".equals(username) && "secret123".equals(password)) {
+            String token = jwTutil.generateToken(username);
+            return ResponseEntity.ok(Map.of("token",token));
+        }
+        return ResponseEntity.status(401).body("Invalid Credentials");
+    }
+    
+}
