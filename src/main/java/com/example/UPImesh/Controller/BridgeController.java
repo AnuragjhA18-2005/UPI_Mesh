9dcc7dc41152826666a4962c35c5a63cfda2ad72
AnuragjhA18-2005 <anuragjha0200@gmail.com>
2026-06-07 22:43:20 +0530

    feat: add BridgeController for bridge communication

---
+++ src/main/java/com/example/UPImesh/Controller/BridgeController.java
@@ -0,0 +1,30 @@
+package com.example.UPImesh.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.UPImesh.Model.MeshPacket;
+import com.example.UPImesh.Service.BridgeIngestionService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/api/bridge")
+public class BridgeController {
+    @Autowired
+    private BridgeIngestionService bridgeIngestionService;
+    
+    @PostMapping("/ingest")
+    public ResponseEntity<String> ingestPacket(@RequestBody MeshPacket packet){
+        String result=bridgeIngestionService.ingest(packet);
+
+        if (result.startsWith("INVALID") || result.startsWith("DUPLICATE")) {
+            return ResponseEntity.badRequest().body(result);
+        }
+        return ResponseEntity.ok(result);
+    }
+    
+}
