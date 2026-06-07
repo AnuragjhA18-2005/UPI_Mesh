3d8923adb77bf1e05950c2fc18bb5f201dd39bd3
AnuragjhA18-2005 <anuragjha0200@gmail.com>
2026-06-07 22:41:40 +0530

    feat: add Account data model

---
+++ src/main/java/com/example/UPImesh/Model/Account.java
@@ -0,0 +1,43 @@
+package com.example.UPImesh.Model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+
+@Entity
+public class Account {
+    @Id
+    private String accountId;
+
+    private BigDecimal balance;
+
+    @Version // prevents double spending from account if two offline packets for the same accound arrive at the same millisecond
+    private Long version;
+
+    public Account() {}
+
+    public Account(String accountId, BigDecimal balance) {
+        this.accountId = accountId;
+        this.balance = balance;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    
+}
